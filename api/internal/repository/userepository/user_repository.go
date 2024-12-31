ab6de836f303ef4b6d55ac6dde338d0eb75a1d15
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-12-31 20:27:58 -0300

    metodo para deletar usuario

--- api/internal/repository/userepository/user_repository.go
+++ api/internal/repository/userepository/user_repository.go
@@ -4,6 +4,7 @@ import (
 	"api/internal/database/sqlc"
 	"api/internal/entity"
 	"context"
+	"database/sql"
 )
 
 func (r *repository) CreateUser(ctx context.Context, u *entity.UserEntity) error {
@@ -37,18 +38,80 @@ func (r *repository) FindUserByEmail(ctx context.Context, email string) (*entity
 }
 
 func (r *repository) FindUserByID(ctx context.Context, id string) (*entity.UserEntity, error) {
-	return nil, nil
+	user, err := r.queries.FindUserByID(ctx, id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	userEntity := entity.UserEntity{
+		ID: user.ID,
+		Name: user.Name,
+		Email: user.Email,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+
+	return &userEntity, nil
 }
 
 func (r *repository) UpdateUser(ctx context.Context, u *entity.UserEntity) error {
+	var name sql.NullString
+	var email sql.NullString
+
+	if u.Name != "" {
+		name = sql.NullString{String: u.Name, Valid: true}
+	} else {
+		name = sql.NullString{Valid: false}
+	}
+
+	if u.Email != "" {
+		email = sql.NullString{String: u.Email, Valid: true}
+	} else {
+		email = sql.NullString{Valid: false}
+	}
+
+	err := r.queries.UpdateUser(ctx, sqlc.UpdateUserParams{
+		ID:        u.ID,
+		Name:      name.String,
+		Email:     email.String,
+		UpdatedAt: u.UpdatedAt,
+	})
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (r *repository) DeleteUser(ctx context.Context, id string) error {
+	err := r.queries.DeleteUser(ctx, id)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (r *repository) FindManyUsers(ctx context.Context) ([]entity.UserEntity, error) {
+	users, err := r.queries.FindManyUsers(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var usersEntity []entity.UserEntity
+
+	for _, user := range users {
+		userEntity := entity.UserEntity {
+			ID: user.ID,
+			Name: user.Name,
+			Email: user.Email,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+		}
+
+		usersEntity = append(usersEntity, userEntity)
+	}
 	return nil, nil
 }
 
