89503c062843c47c30e2fead8c8799a4aad8ea15
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2025-01-05 14:53:36 -0300

    criando usuario

--- api/internal/service/userservices/user_service.go
+++ api/internal/service/userservices/user_service.go
@@ -5,6 +5,7 @@ import (
 	"api/internal/entity"
 	"api/internal/response"
 	"context"
+	"database/sql"
 	"errors"
 	"log/slog"
 	"time"
@@ -14,39 +15,41 @@ import (
 )
 
 func (s *service) CreateUser(ctx context.Context, u dto.CreateUserDto) error {
-	userExist, err := s.repository.FindUserByEmail(ctx, u.Email)
-	if err != nil {
-		slog.Error("error to search user by email", "err", err, slog.String("package", "userservices"))
-		return err
-	}
+    userExist, err := s.repository.FindUserByEmail(ctx, u.Email)
+    
+    if err != nil && err != sql.ErrNoRows {
+        slog.Error("error to search user by email", "err", err, slog.String("package", "userservices"))
+        return err
+    }
 
-	if userExist != nil {
-		slog.Error("user already exists", slog.String("package", "userservices"))
-		return errors.New("user already exists")
-	}
+    if err == nil && userExist.Email != "" {
+        slog.Error("user already exists", slog.String("package", "userservices"))
+        return errors.New("user already exists")
+    }
 
-	passwordEncrypted, err := bcrypt.GenerateFromPassword([]byte(u.Password), 12)
-	if err != nil {
-		slog.Error("error to encrypt password", "err", err, slog.String("package", "userservices"))
-		return errors.New("error to encrypt password")
-	}
+    passwordEncrypted, err := bcrypt.GenerateFromPassword([]byte(u.Password), 12)
+    if err != nil {
+        slog.Error("error to encrypt password", "err", err, slog.String("package", "userservices"))
+        return errors.New("error to encrypt password")
+    }
 
-	newUser := entity.UserEntity {
-		ID: uuid.New().String(),
-		Name: 	u.Name,
-		Email: u.Email,
-		Password: string(passwordEncrypted),
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
+    newUser := entity.UserEntity{
+        ID: uuid.New().String(),
+        Name: u.Name,
+        Email: u.Email,
+        Password: string(passwordEncrypted),
+        CreatedAt: time.Now(),
+        UpdatedAt: time.Now(),
+    }
 
-	err = s.repository.CreateUser(ctx, &newUser)
-	if err != nil {
-		slog.Error("error to create user", "err", err, slog.String("package", "userservices"))
-		return err
-	}
+    err = s.repository.CreateUser(ctx, &newUser)
+    if err != nil {
+        slog.Error("error to create user", "err", err, slog.String("package", "userservices"))
+        return errors.New("failed to create user in database")
+    }
 
-	return nil
+    slog.Info("user successfully created", slog.String("email", u.Email))
+    return nil
 }
 
 func (s *service) UpdateUser(ctx context.Context, u dto.UpdateUserDto, id string) error {
@@ -166,24 +169,30 @@ func (s *service) DeleteUser(ctx context.Context, id string) error {
 }
 
 func (s *service) UpdateUserPassword(ctx context.Context, u *dto.UpdateUserPasswordDto, id string) error {
-	userExists, err := s.repository.FindUserByID(ctx, id)
+	// userExists, err := s.repository.FindUserByID(ctx, id)
+	// if err != nil {
+	// 	slog.Error("error to search user by id", "err", err, slog.String("package", "userservice"))
+	// 	return err
+	// }
+	// if userExists == nil {
+	// 	slog.Error("user not found", slog.String("package", "userservice"))
+	// 	return errors.New("user not found")
+	// }
+
+	oldPass, err := s.repository.GetUserPassword(ctx, id)
 	if err != nil {
-		slog.Error("error to search user by id", "err", err, slog.String("package", "userservice"))
+		slog.Error("error to get user password", "err", err, slog.String("package", "userservice"))
 		return err
 	}
-	if userExists == nil {
-		slog.Error("user not found", slog.String("package", "userservice"))
-		return errors.New("user not found")
-	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(userExists.Password), []byte(u.OldPassword))
+	err = bcrypt.CompareHashAndPassword([]byte(oldPass), []byte(u.OldPassword))
 
 	if err != nil {
 		slog.Error("invalid password", slog.String("package", "userservice"))
     	return errors.New("invalid password")
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(userExists.Password), []byte(u.Password))
+	err = bcrypt.CompareHashAndPassword([]byte(oldPass), []byte(u.Password))
 	if err == nil {
 		slog.Error("new password is equal to old password", slog.String("package", "userservice"))
 		return errors.New("new password is equal to old password")
