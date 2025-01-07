653c6c9ef2d24a1c359cbb4dc9373bf736a20128
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2025-01-07 11:27:02 -0300

    finalizado

--- api/internal/handler/routes/user_routes.go
+++ api/internal/handler/routes/user_routes.go
@@ -1,29 +1,31 @@
 package routes
 
 import (
+	"api/internal/config/env"
 	"api/internal/handler/middleware"
 	"api/internal/handler/userhandler"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth/v5"
 )
 
 func InitUserRoutes(router chi.Router, h userhandler.UserHandler) {
 	router.Use(middleware.LoggerData)
 
-	router.Post("/user", h.CreateUser)
+	router.Post("/create-user", h.CreateUser)
 	
-	// router.Route("/user", func(r chi.Router){
-	// 	r.Use(jwtauth.Verifier(env.Env.TokenAutn))
-	// 	r.Use(jwtauth.Authenticator(env.Env.TokenAutn))
+	router.Route("/user", func(r chi.Router){
+		r.Use(jwtauth.Verifier(env.Env.TokenAutn))
+		r.Use(jwtauth.Authenticator(env.Env.TokenAutn))
 		
-	// 	r.Patch("/{id}", h.UpdateUser)
-	// 	r.Get("/{id}", h.GetUserByID)
-	// 	r.Delete("/{id}", h.DeleteUser)
-	// 	r.Get("/list-all", h.FindManyUsers)
-	// 	r.Patch("/password/{id}", h.UpdateUserPassword)
-	// })
+		r.Patch("/{id}", h.UpdateUser)
+		r.Get("/{id}", h.GetUserByID)
+		r.Delete("/{id}", h.DeleteUser)
+		r.Get("/list-all", h.FindManyUsers)
+		r.Patch("/password/{id}", h.UpdateUserPassword)
+	})
 
-	// router.Route("/auth", func(r chi.Router){
-	// 	r.Post("/login", h.Login)
-	// })
+	router.Route("/auth", func(r chi.Router){
+		r.Post("/login", h.Login)
+	})
 }
